8c486f701fff4329f8f98d749054aaeb7460ebc2
mrmanoj950 <mrmanoj950@gmail.com>
2025-02-23 01:12:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,14 +13,14 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+API_ID = int(getenv("API_ID", "23069582"))
+API_HASH = getenv("API_HASH", "b3b56eaf67828684f54d540f684fdf1f")
+BOT_TOKEN = getenv("BOT_TOKEN", "7672004632:AAHohyBfssnv2Nlscz0tqiji3r1I7pfsdUc")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "1780523256").split()))
 MONGO_DB = getenv("MONGO_DB", "")
 LOG_GROUP = getenv("LOG_GROUP", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002321329063"))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "1999"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
 AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
